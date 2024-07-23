25391128571bddfcf067de26a3d227718ff52356
Marcos Vinicius <157753545+MVLRGTR@users.noreply.github.com>
2024-07-23 11:18:30 -0300

    inicio form register

--- Javascript/Curso NodeJs/15_Get_a_Pet/frontend/src/App.js
+++ Javascript/Curso NodeJs/15_Get_a_Pet/frontend/src/App.js
@@ -1,23 +1,26 @@
-import {BrowserRouter as Router ,Routes,Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from './components/pages/auth/Login'
 import Register from './components/pages/auth/Register';
 import Home from './components/pages/Home';
 /*Components */
 import Navbar from './components/Layouts/Navbar';
 import Footer from './components/Layouts/Footer';
+import Container from './components/Layouts/Container'
 
 function App() {
   return (
     <Router>
-      <Navbar/>
-      <Routes>
+      <Navbar />
+      <Container>
+        <Routes>
 
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/register' element={<Register/>}/>
-        <Route path='/' element={<Home/>}/>
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/' element={<Home />} />
 
-      </Routes>
-      <Footer/>
+        </Routes>
+      </Container>
+      <Footer />
     </Router>
   );
 }
--- Javascript/Curso NodeJs/15_Get_a_Pet/frontend/src/components/pages/auth/Register.js
+++ Javascript/Curso NodeJs/15_Get_a_Pet/frontend/src/components/pages/auth/Register.js
@@ -1,7 +1,24 @@
+import Input from "../../form/Input"
+import styles from '../../../components/form/Form.module.css'
+import { Link } from "react-router-dom"
 function Register(){
+    function handleChange(event){
+
+    }
     return(
-        <section>
-            <h1>Register</h1>
+        <section className={styles.form_container}>
+            <h1>Registrar</h1>
+            <form>
+                <Input text='Nome' type='text' name='name' placeholder='Digite seu nome' handleOnChange={handleChange}></Input>
+                <Input text='Telefone' type='text' name='phone' placeholder='Digite seu telefone' handleOnChange={handleChange}></Input>
+                <Input text='E-mail' type='email' name='email' placeholder='Digite seu E-mail' handleOnChange={handleChange}></Input>
+                <Input text='Senha' type='password' name='password' placeholder='Digite sua Senha' handleOnChange={handleChange}></Input>
+                <Input text='Confirmação de Senha' type='password' name='confirmpassword' placeholder='Digite sua Senha novamente' handleOnChange={handleChange}></Input>
+                <input type='submit' value='Cadastrar'/>
+            </form>
+            <p>
+                Já tem conta ? <Link to='/login'>Clique aqui</Link>
+            </p>
         </section>
     )
 }
