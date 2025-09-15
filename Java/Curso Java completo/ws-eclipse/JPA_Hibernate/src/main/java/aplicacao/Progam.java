0476200cafd0a63e80fd17e82054bb6ab4727816
Marcos Vincius <mvlrgtr@outlook.com>
2025-09-15 11:53:27 -0300

    Adicionando arquivo PDF e atualizando classes de JPA

--- Java/Curso Java completo/ws-eclipse/JPA_Hibernate/src/main/java/aplicacao/Progam.java
+++ Java/Curso Java completo/ws-eclipse/JPA_Hibernate/src/main/java/aplicacao/Progam.java
@@ -1,17 +1,39 @@
 package aplicacao;
 
 import dominio.Pessoas;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 
 public class Progam {
 
 	public static void main(String[] args) {
-		Pessoas pessoa = new Pessoas(1,"Carlos Santos Silva","carlos@gmail.com");
-		Pessoas pessoa2 = new Pessoas(2,"Cesar Lopes","cesar@gmail.com");
-		Pessoas pessoa3 = new Pessoas(3,"Murilo Gomes","murilo@gmail.com");
 		
-		System.out.println(pessoa);
-		System.out.println(pessoa2);
-		System.out.println(pessoa3);
+		/*Pessoas pessoa = new Pessoas(null,"Carlos Santos Silva","carlos@gmail.com");
+		Pessoas pessoa2 = new Pessoas(null,"Cesar Lopes","cesar@gmail.com");
+		Pessoas pessoa3 = new Pessoas(null,"Murilo Gomes","murilo@gmail.com");
+		*/
+		
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		/*em.getTransaction().begin(); // no jpa utilizamos o beggin e commit para definir uma transação no bd
+		em.persist(pessoa);
+		em.persist(pessoa2);
+		em.persist(pessoa3);
+		em.getTransaction().commit();
+		*/
+		
+		//o jpa só remove um objeto que está monitorado no contexto , ou seja eu não posso instnaciar o objeto e mandar o jpa remover
+		//tenho que puxar do banco de dados Pessoas p = em.find(Pessoas.class,2); e depois em.remove(p);
+		
+		Pessoas p = em.find(Pessoas.class, 2);
+		System.out.println(p);
+		
+		System.out.println("Finished");
+		em.close();
+		emf.close();
 		
 	}
 
